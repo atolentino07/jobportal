c187c50b7d98edfa93257f2d104fd0fb75cd0107
atolentino07 <102979320+atolentino07@users.noreply.github.com>
2023-12-10 21:12:15 -0800

    Add files via upload

---
+++ About.php
@@ -0,0 +1,161 @@
+ 
+
+<section id="content">
+	<section class="section-padding">
+		<div class="container">
+			<div class="row showcase-section">
+				<div class="col-md-6">
+				 <img src="<?php echo web_root; ?>plugins/home-plugins/img/suitcase.png" alt="" />
+				</div>
+				<div class="col-md-6">
+					<div class="about-text">
+						<h3>PATHS</h3>
+						 <p>Offering a spectrum of opportunities tailored to your ambitions. Embrace the freedom to choose your path, and let us guide you toward success. Your unique path starts here.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="container">
+					
+					<div class="about">
+				
+						
+						<div class="row">
+							<div class="col-md-4">
+								<!-- Heading and para -->
+								<div class="block-heading-two">
+									<h3><span>Why Choose Us?</span></h3>
+								</div>
+								<p>
+								Explore endless opportunities on our website where innovation meets opportunity. Join a dynamic community of professionals shaping the future. 
+								Your dream job awaits – apply now and embark on a rewarding journey with us!
+								</p>
+							</div>
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Solution</span></h3>
+								</div>		
+								<!-- Accordion starts -->
+								<div class="panel-group" id="accordion-alt3">
+								 <!-- Panel. Use "panel-XXX" class for different colors. Replace "XXX" with color. -->
+								  <div class="panel">	
+									<!-- Panel heading -->
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+											<i class="fa fa-angle-right"></i> Mission/Vision
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseOne-alt3" class="panel-collapse collapse">
+										<!-- Panel body -->
+										<div class="panel-body">
+										Having various companies from various industries to serve as medium for potential clients and applicants that seeks opportunity in their expertise. 
+										</div>
+									 </div>
+								  </div>
+								</div>
+								<!-- Accordion ends -->
+								
+							</div>
+							
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Expertise</span></h3>
+								</div>								
+								<h6>Web Development</h6>
+								<div class="progress pb-sm">
+								  <!-- White color (progress-bar-white) -->
+								  <div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>Designing</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>User Experience</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-lblue" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>Development</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+							</div>
+							
+						</div>
+						
+						 						
+						 
+						<br>
+						<!-- Our Team starts -->
+				
+						<!-- Heading -->
+						<div class="block-heading-six">
+							<h4 class="bg-color">Our Team</h4>
+						</div>
+						<br>
+						
+						<!-- Our team starts -->
+						
+						<div class="team-six">
+							<div class="row">
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team1.jpg" alt="">
+										<!-- Name -->
+										<h4>Dan Francisco</h4>
+										<span class="deg">Project Head</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team2.jpg" alt="">
+										<!-- Name -->
+										<h4>Anzel Tolentino</h4>
+										<span class="deg">Programmer</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team3.jpg" alt="">
+										<!-- Name -->
+										<h4>Harlene Pallalos</h4>
+										<span class="deg">Data Entry</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team4.jpg" alt="">
+										<!-- Name -->
+										<h4>John Clarence</h4>
+										<span class="deg">wala hahaha</span> 
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Our team ends -->
+					  
+						
+					</div>
+									
+				</div>
+	</section> 
+ 
---
+++ home.php
@@ -0,0 +1,134 @@
+  <section id="banner">
+   
+  <!-- Slider -->
+        <div id="main-slider" class="flexslider">
+            <ul class="slides">
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/1.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3>innovation</h3> 
+          <p>We create the opportunities</p> 
+           
+                </div>
+              </li>
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/2.jpg" alt="" />
+                <div class="flex-caption">
+                    <h3>Specialize</h3> 
+          <p>Success depends on work</p> 
+           
+                </div>
+              </li>
+            </ul>
+        </div>
+  <!-- end slider -->
+ 
+  </section> 
+  <section id="call-to-action-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-sm-9">
+          <h3>Partner with Business Leaders</h3>
+          <p>
+		  Establishing enduring, effective strategic partnerships between customers and suppliers, centered around the attainment of optimal practices and sustainable competitive benefits, is a key focus. 
+		  Within the business partner framework, HR professionals collaborate closely with business leaders and line managers to realize mutual organizational goals
+		  </p>
+        </div>
+       <!--  <div class="col-md-2 col-sm-3">
+          <a href="#" class="btn btn-primary">Read More</a>
+        </div> -->
+      </div>
+    </div>
+  </section>
+  
+  <section id="content">
+  
+  
+  <div class="container">
+        <div class="row">
+      <div class="col-md-12">
+        <div class="aligncenter"><h2 class="aligncenter">Company</h2><!-- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, doloribus omnis minus ovident, doloribus omnis minus temporibus perferendis nesciunt.. --></div>
+        <br/>
+      </div>
+    </div>
+
+    <?php 
+	
+	/*---------------------------------SQL STATEMENT HERE------------------------------------------------------------*/
+    /*--------------------*/  $sql = "SELECT * FROM `tblcompany`"; /*--------------------*/
+	/*---------------------------------SQL STATEMENT HERE------------------------------------------------------------*/
+	
+      $mydb->setQuery($sql);
+      $comp = $mydb->loadResultList();
+
+
+      foreach ($comp as $company ) {
+        # code...
+    
+    ?>
+            <div class="col-sm-4 info-blocks">
+                <i class="icon-info-blocks fa fa-building-o"></i>
+                <div class="info-blocks-in">
+                    <h3><?php echo $company->COMPANYNAME;?></h3>
+                    <!-- <p><?php echo $company->COMPANYMISSION;?></p> -->
+                    <p>Address :<?php echo $company->COMPANYADDRESS;?></p>
+                    <p>Contact No. :<?php echo $company->COMPANYCONTACTNO;?></p>
+                </div>
+            </div>
+
+    <?php } ?> 
+  </div>
+  </section>
+  
+  <section class="section-padding gray-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-title text-center">
+            <h2 >Popular Jobs</h2>  
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 ">
+          <?php 
+	/*---------------------------------SQL STATEMENT HERE------------------------------------------------------------*/
+    /*--------------------*/        $sql = "SELECT * FROM `tblcategory`"; /*--------------------*/
+	/*---------------------------------SQL STATEMENT HERE------------------------------------------------------------*/
+	
+            $mydb->setQuery($sql);
+            $cur = $mydb->loadResultList();
+
+            foreach ($cur as $result) {
+              echo '<div class="col-md-3" style="font-size:15px;padding:5px">* <a href="'.web_root.'index.php?q=category&search='.$result->CATEGORY.'">'.$result->CATEGORY.'</a></div>';
+            }
+
+          ?>
+        </div>
+      </div>
+ 
+    </div>
+  </section>    
+  <section id="content-3-10" class="content-block data-section nopad content-3-10">
+  <div class="image-container col-sm-6 col-xs-12 pull-left">
+    <div class="background-image-holder">
+
+    </div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6 col-sm-offset-6 col-xs-12 content">
+        <div class="editContent">
+          <h3>Our Team</h3>
+        </div>
+        <div class="editContent"  style="height:235px;">
+          <p> 
+          Exemplifying a culture of unwavering work ethics, our stellar team sets the gold standard for excellence in the corporate realm. Together, we redefine dedication and professionalism, driving success with integrity at the heart of every endeavor.</p>
+        </div> 
+      </div>
+    </div><!-- /.row-->
+  </div><!-- /.container -->
+</section>
+  
+  
